cee2727d2701ad1c5a8c69d8c941d03486209786
elikawa7 <metall65eli@gmail.com>
2023-08-17 01:28:22 -0400

    setting some keywords for identifying tokentype

--- src/main/java/shdlang/lexer/KeyWords.java
+++ src/main/java/shdlang/lexer/KeyWords.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KeyWords {
-    public static final Map<String, TokenType> KEYWORDS = new HashMap<>();
+    private static final Map<String, TokenType> KEYWORDS = new HashMap<>();
+    public static TokenType get(String key){
+        return KEYWORDS.get(key);
+    }
     static {
         KEYWORDS.put("let", TokenType.LET);
     }
