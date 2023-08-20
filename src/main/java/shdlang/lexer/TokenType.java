098856b5cfee328c608b67088bf65aa5ed431645
elikawa7 <metall65eli@gmail.com>
2023-08-19 21:47:49 -0400

    all types added

--- src/main/java/shdlang/lexer/TokenType.java
+++ src/main/java/shdlang/lexer/TokenType.java
@@ -1,14 +1,15 @@
 package shdlang.lexer;
 
 public enum TokenType {
-    // Keywords
-    LET,
+    //single-character tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-    // Grouping * Operators
-    BINARYOPERATOR,
-    EQUALS,
-    OPENPAREN,
-    ClOSEPAREN,
-    NUMBER,
-    IDENTIFIER
+    //one or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    //literals
+    IDENTIFIER, STRING, NUMBER,
+
+    //Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF
 }
