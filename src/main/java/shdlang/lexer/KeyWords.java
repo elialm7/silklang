3a289d1f8d946cd13ebfd75ff6efa3c1406f2a45
elikawa7 <metall65eli@gmail.com>
2023-08-19 21:42:24 -0400

    added new reserved keywords

--- src/main/java/shdlang/lexer/KeyWords.java
+++ src/main/java/shdlang/lexer/KeyWords.java
@@ -3,12 +3,30 @@ package shdlang.lexer;
 import java.util.HashMap;
 import java.util.Map;
 
+import static shdlang.lexer.TokenType.*;
+
+
 public class KeyWords {
-    private static final Map<String, TokenType> KEYWORDS = new HashMap<>();
+    private static final Map<String, TokenType> keywords = new HashMap<>();
     public static TokenType get(String key){
-        return KEYWORDS.get(key);
+        return keywords.get(key);
     }
     static {
-        KEYWORDS.put("let", TokenType.LET);
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
     }
 }
