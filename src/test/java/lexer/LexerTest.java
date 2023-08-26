dc0bb76cedaaaa93f03eb0fbcf207a9b7201d6f1
elikawa7 <metall65eli@gmail.com>
2023-08-26 17:29:03 -0400

    name of test changed

--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -5,14 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import shdlang.lexer.ShdLexer;
 import shdlang.lexer.Token;
+import shdlang.lexer.TokenType;
 
 import java.util.List;
 
 public class LexerTest {
-
     @Test
-    public void testTokenize(){
-
+    public void testVariableDeclaration(){
         String testcode = "var hola = \"hello\" ";
         System.out.println(testcode);
         ShdLexer lexer = new ShdLexer(testcode);
