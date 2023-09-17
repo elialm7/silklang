a787c67277bba97d9b6f6743f7e2b3e2c38ca796
elikawa7 <metall65eli@gmail.com>
2023-09-17 01:28:24 -0400

    added the visitvar method for visitin the ast.

--- src/main/java/silklang/ParserRepresentation/Statement/base/StmtVisitor.java
+++ src/main/java/silklang/ParserRepresentation/Statement/base/StmtVisitor.java
@@ -7,10 +7,12 @@ package silklang.ParserRepresentation.Statement.base;
 
 import silklang.ParserRepresentation.Statement.Representation.Expression;
 import silklang.ParserRepresentation.Statement.Representation.Print;
+import silklang.ParserRepresentation.Statement.Representation.Var;
 
 public interface StmtVisitor<T>{
 
     T visitExpressionStmt(Expression expr);
     T visitPrintStmt(Print pr);
+    T visitVarStmt(Var vr);
 
 }
