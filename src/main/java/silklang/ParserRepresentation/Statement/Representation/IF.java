bfcb844f4cb7d762d66e24771ed80e80225990c3
elikawa7 <metall65eli@gmail.com>
2023-09-18 19:23:46 -0400

    added if class representation and visitor

---
+++ src/main/java/silklang/ParserRepresentation/Statement/Representation/IF.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) under GPL V3. Read LICENSE located in the root of the project.
+ * All rights reserved.
+ */
+
+package silklang.ParserRepresentation.Statement.Representation;
+
+import silklang.ParserRepresentation.Expressions.base.Expr;
+import silklang.ParserRepresentation.Statement.base.Stmt;
+import silklang.ParserRepresentation.Statement.base.StmtVisitor;
+
+public class IF extends Stmt {
+
+    private Expr condition;
+    private Stmt thenBranch;
+    private Stmt elseBranch;
+
+    public IF(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+        this.condition = condition;
+        this.thenBranch = thenBranch;
+        this.elseBranch = elseBranch;
+    }
+
+    public Expr getCondition() {
+        return condition;
+    }
+
+    public Stmt getThenBranch() {
+        return thenBranch;
+    }
+
+    public Stmt getElseBranch() {
+        return elseBranch;
+    }
+
+    @Override
+    public <T> T accept(StmtVisitor<T> visitor) {
+        return visitor.visitIfStmt(this);
+    }
+}
--- src/main/java/silklang/ParserRepresentation/Statement/base/StmtVisitor.java
+++ src/main/java/silklang/ParserRepresentation/Statement/base/StmtVisitor.java
@@ -5,10 +5,7 @@
 
 package silklang.ParserRepresentation.Statement.base;
 
-import silklang.ParserRepresentation.Statement.Representation.Block;
-import silklang.ParserRepresentation.Statement.Representation.Expression;
-import silklang.ParserRepresentation.Statement.Representation.Print;
-import silklang.ParserRepresentation.Statement.Representation.Var;
+import silklang.ParserRepresentation.Statement.Representation.*;
 
 public interface StmtVisitor<T>{
 
@@ -17,5 +14,6 @@ public interface StmtVisitor<T>{
     T visitVarStmt(Var vr);
     T visitBlockStmt(Block bl);
 
+    T visitIfStmt(IF ifstmt);
 
 }
