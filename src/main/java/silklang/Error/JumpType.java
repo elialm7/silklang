539ff9ba63972e52830a86c10847a96f05add9fc
elikawa7 <metall65eli@gmail.com>
2023-09-18 23:59:50 -0400

    jump types that can occur in a loop.

---
+++ src/main/java/silklang/Error/JumpType.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) under GPL V3. Read LICENSE located in the root of the project.
+ * All rights reserved.
+ */
+
+package silklang.Error;
+
+public enum JumpType {
+	 BREAK, CONTINUE;
+}
