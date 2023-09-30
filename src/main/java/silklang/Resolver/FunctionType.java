b787960aef1d747d7de8c0719fe0e173b20ea4dd
elikawa7 <metall65eli@gmail.com>
2023-09-30 16:22:46 -0400

    enum for the types of functions

---
+++ src/main/java/silklang/Resolver/FunctionType.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) under GPL V3. Read LICENSE located in the root of the project.
+ * All rights reserved.
+ */
+
+package silklang.Resolver;
+
+public enum FunctionType {
+
+    NONE, FUNCTION
+
+}
