32e0b2255e356fa680f11bf5e60a6cc1d0c8e8b1
elikawa7 <metall65eli@gmail.com>
2023-09-30 16:07:31 -0400

    added cli options and implementation for those.

--- src/main/java/silklang/App/Silk.java
+++ src/main/java/silklang/App/Silk.java
@@ -8,36 +8,89 @@
 //bom dia
 package silklang.App;
 
+import org.apache.commons.cli.*;
 import silklang.Error.RuntimeError;
 import silklang.Interpreter.Interpreter;
 import silklang.Lexer.SilkLexer;
 import silklang.Lexer.Token;
 import silklang.Lexer.TokenType;
+import silklang.Linker.SilkLinker;
 import silklang.Parser.SilkParser;
 import silklang.ParserRepresentation.Statement.base.Stmt;
+import silklang.Resolver.Resolver;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class Silk{
     private static final Interpreter interpreter = new Interpreter();
     static boolean haderror = false;
     static boolean hadRuntimeError = false;
+
+    public static Options getOptions(){
+        Options interpreterOptions = new Options();
+        Option InterpretFile = Option.builder("r").longOpt("run").hasArg(true)
+                .argName("Archivo silk").desc("Interprete un archivo con codigo silk.").build();
+        Option InterpretOnly = Option.builder("repl").hasArg(false).desc("Entra en el REPL[READ - EVAL - PRINT - LOOP]").build();
+        Option InterpreteFileDesc = Option.builder("mr").longOpt("mrun")
+                .hasArg(true).argName("Archivo descriptor silk").desc("Lee un archivo de descripcion y luego interpreta las dependencias. ").build();
+        Option helpOption = Option.builder("h").hasArg(false).desc("Muesta la lista de ayuda").build();
+        interpreterOptions.addOption(InterpretFile).addOption(InterpretOnly).addOption(InterpreteFileDesc).addOption(helpOption);
+        return interpreterOptions;
+    }
+
+
     public static void main(String[] args) throws IOException{
-        if(args.length > 1){
-            System.out.println("Uso: [script] ");
-            System.exit(64);
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        Options interpreterOptions = getOptions();
+        try{
+            if(args.length == 0) throw new ParseException("Se debe pasar almenos una opcion. ");
+            cmd = parser.parse(interpreterOptions, args);
+            if(cmd.hasOption("r")){
+                String silkFile = cmd.getOptionValue("r");
+                runFile(silkFile);
+            }else if(cmd.hasOption("run")){
+                String silkFile = cmd.getOptionValue("run");
+                runFile(silkFile);
+            }else if(cmd.hasOption("repl")){
+                runPromt();
+            }else if(cmd.hasOption("mr")){
+                String makeFile = cmd.getOptionValue("mr");
+                runMakeFile(makeFile);
+            }else if(cmd.hasOption("mrun")){
+                String makeFile = cmd.getOptionValue("mrun");
+                runMakeFile(makeFile);
+            }else if(cmd.hasOption("h")){
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("Ayuda", interpreterOptions);
+                System.exit(0);
+            }
+
+        }catch (ParseException ex){
+            System.out.println("Se produjo un error parseando. "+ex.getMessage());
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("Ayuda", interpreterOptions);
+            System.exit(0);
         }
-        if(args.length == 1 ){
-            runFile(args[0]);
-        }else{
-            runPromt();
+    }
+
+    private static void runMakeFile(String source) throws IOException {
+        var linker = new SilkLinker(new File(source).toURI());
+        var sourceBuilder = new StringBuilder();
+        var filestoLink = linker.getDependencies();
+        for(var arch: Collections.unmodifiableList(filestoLink)){
+            var lines = Files.readAllLines(arch.toPath());
+            sourceBuilder.append(String.join("", lines));
         }
+        run(sourceBuilder.toString());
     }
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
@@ -50,7 +103,7 @@ public class Silk{
     private static void runPromt() throws IOException{
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
-        System.out.println("SilkLang-Interpreter V1.0.0");
+        System.out.println("SilkLang-Interpreter Ultimate");
         System.out.println("Para salir escriba 'exit'");
         System.out.println("Copyright (c) under GPL V3.");
         String line = "";
@@ -71,6 +124,9 @@ public class Silk{
         SilkParser parser = new SilkParser(tokens);
         List<Stmt> statements = parser.parse();
         if(haderror) return;
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        if(haderror)return;
         interpreter.interpret(statements);
     }
 
